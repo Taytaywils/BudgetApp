b0d5753af6222129b6bf08ef2bd04bd2ad5d6ec9
Taytaywils <wilsjtay@gmail.com>
2025-11-07 22:22:10 -0700

    Halloween Theme

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,16 @@
 const btn = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
+const toggleBtn = document.getElementById('theme-toggle');
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('visible');
     btn.classList.toggle('toggle');
+});
+
+
+const theme = document.getElementById('theme-link');
+toggleBtn.addEventListener('click', () => {
+    const isDark = theme.disabled;
+    theme.disabled = !isDark;
+    localStorage.setItem('darkMode', !isDark);
 });
